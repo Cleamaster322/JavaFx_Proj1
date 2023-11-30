192aff03a085600056f6f4f36184225f7b120183
Cleamaster322 <82811442+Cleamaster322@users.noreply.github.com>
2023-11-30 14:22:07 +1000

    test 3

--- demo1/src/main/java/module-info.java
+++ demo1/src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-    requires org.slf4j.simple;
+    requires org.slf4j;
     requires org.xerial.sqlitejdbc;
 
 
