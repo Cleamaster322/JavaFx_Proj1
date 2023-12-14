84386d237b7a6ed16526bcfc4d9a038cea18a71f
bennnnnja <nikitayurov95@gmail.com>
2023-12-14 18:21:24 +1000

    added the ability to add to favorites

--- demo1/src/main/java/com/example/demo1/HelloController.java
+++ demo1/src/main/java/com/example/demo1/HelloController.java
@@ -10,7 +10,6 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import java.sql.SQLException;
 import java.util.List;
-
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.geometry.Pos;
@@ -19,7 +18,6 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicBoolean;
-
 
 public class HelloController extends DataBaseHandler { //ожидает передачи интерфейса, фейкового класса. Но нужно получить объект, который наследуется у интерфейса и нужен класс, который управляет зависимостями у интерфейса.
     //паттерн depencity inject
@@ -70,10 +68,34 @@ public class HelloController extends DataBaseHandler { //ожидает пере
                 int index = row * 3 + col;
                 Button button = new Button("Блюдо " + (index + 1));
                 button.setPrefSize(150, 100);
-                gridPane.add(button, col, row);
+
+                StackPane buttonContainer = new StackPane(); // Используем StackPane для перекрытия кнопок
+                buttonContainer.setAlignment(Pos.TOP_RIGHT);
+
+                Button heartButton = new Button("A");
+                heartButton.setPrefSize(20, 20);
+
+                heartButton.setUserData(button);
+                // Добавляем кнопку с сердечком в правый верхний угол основной кнопки
+                buttonContainer.getChildren().addAll(button, heartButton);
+
+                // Обработчик события нажатия на кнопку с сердечком
+                heartButton.setOnAction(e -> {
+                    // Копируем основную кнопку в окно "Избранное"
+                    Button selectedButton = (Button) heartButton.getUserData();
+                    addFavoriteButtonAction(selectedButton);
+                });
+
+                gridPane.add(buttonContainer, col, row);
             }
         }
     }
+
+    private void addFavoriteButtonAction(Button selectedButton){
+        Button clonedButton = new Button(selectedButton.getText());
+        favoritesBox.getChildren().add(clonedButton);
+    }
+
 
     private void addCitiesToEmptyPane(Pane emptyPane, List<String> cities) {
         VBox cityContainer = new VBox();
@@ -117,54 +139,47 @@ public class HelloController extends DataBaseHandler { //ожидает пере
     }
 
     private void addProductComboBox(GridPane gridPane) {
-        // Создаем новый ComboBox
         ComboBox<String> productComboBox = new ComboBox<>();
 
-        // Создаем список продуктов
-        ObservableList<String> products = FXCollections.observableArrayList(
-                "Бекон",
-                "Яйцо",
-                "Картофель",
-                "Творог",
-                "Сыр",
-                "Хлеб"
-        );
+        try {
+            List<String> cities = GetNotAllData();
+            ObservableList<String> cityList = FXCollections.observableArrayList(cities);
 
-        // Создаем FilteredList, который будет фильтровать продукты
-        FilteredList<String> filteredProducts = new FilteredList<>(products, p -> true);
+            FilteredList<String> filteredProducts = new FilteredList<>(cityList, p -> true);
 
-        // Устанавливаем FilteredList в качестве элементов ComboBox
-        productComboBox.setItems(filteredProducts);
+            productComboBox.setItems(filteredProducts);
 
-        // Устанавливаем ComboBox как редактируемый
-        productComboBox.setEditable(true);
 
-        // Добавляем слушатель к свойству textProperty ComboBox
-        productComboBox.getEditor().textProperty().addListener((obs, oldValue, newValue) -> {
-            // Если выбранный элемент равен текущему вводу, то не фильтруем список
-            if (productComboBox.getSelectionModel().getSelectedItem() == null ||
-                    !productComboBox.getSelectionModel().getSelectedItem().equals(productComboBox.getEditor().getText())) {
-                // Фильтруем список продуктов
-                filteredProducts.setPredicate(product -> product.toLowerCase().contains(newValue.toLowerCase()));
-            }
-        });
+            productComboBox.setEditable(true);
 
-        // Добавляем ComboBox в GridPane
-        gridPane.add(productComboBox, 0, 0);
+            // Добавляем слушатель к свойству textProperty ComboBox
+            productComboBox.getEditor().textProperty().addListener((obs, oldValue, newValue) -> {
+                // Если выбранный элемент равен текущему вводу, то не фильтруем список
+                if (productComboBox.getSelectionModel().getSelectedItem() == null ||
+                        !productComboBox.getSelectionModel().getSelectedItem().equals(productComboBox.getEditor().getText())) {
+                    // Фильтруем список продуктов
+                    filteredProducts.setPredicate(product -> product.toLowerCase().contains(newValue.toLowerCase()));
+                }
+            });
 
-        // Устанавливаем обработчик событий для ComboBox
-        productComboBox.setOnAction(e -> {
-            String selectedProduct = productComboBox.getValue();
-            if (selectedProduct != null && !selectedProduct.isEmpty() && products.contains(selectedProduct)) {
-                System.out.println("Выбран продукт: " + selectedProduct);
-                Platform.runLater(() -> {
-                    if (!productComboBox.getItems().isEmpty()) {
-                        productComboBox.getSelectionModel().clearSelection(); // Снятие выбора
-                    }
-                });
-            }
-        });
+            // Добавляем ComboBox в GridPane
+            gridPane.add(productComboBox, 0, 0);
 
+            // Устанавливаем обработчик событий для ComboBox
+            productComboBox.setOnAction(e -> {
+                String selectedCity = productComboBox.getValue();
+                if (selectedCity != null && !selectedCity.isEmpty() && cities.contains(selectedCity)) {
+                    System.out.println("Выбран продукт: " + selectedCity);
+                    Platform.runLater(() -> {
+                        if (!productComboBox.getItems().isEmpty()) {
+                            productComboBox.getSelectionModel().clearSelection(); // Снятие выбора
+                        }
+                    });
+                }
+            });
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
@@ -187,7 +202,7 @@ public class HelloController extends DataBaseHandler { //ожидает пере
         }
     }
 
-
+    private VBox favoritesBox = new VBox();
 
     public void onBButtonClick() {
         Stage categoryAStage = new Stage();
@@ -197,6 +212,8 @@ public class HelloController extends DataBaseHandler { //ожидает пере
 
         Scene categoryAScene = new Scene(root, 400, 300);
         categoryAStage.setScene(categoryAScene);
+
+        root.getChildren().add(favoritesBox);
 
         categoryAStage.show();
     }
