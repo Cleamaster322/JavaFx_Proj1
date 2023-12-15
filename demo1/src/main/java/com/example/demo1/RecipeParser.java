fcaa7d1c0fc4a46302ee1aa9f261f46d0643318d
Kim Alexander <kim.aeduard@students.dvfu.ru>
2023-12-15 19:53:54 +1000

    categories

--- demo1/src/main/java/com/example/demo1/RecipeParser.java
+++ demo1/src/main/java/com/example/demo1/RecipeParser.java
@@ -3,7 +3,10 @@ package com.example.demo1;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class RecipeParser {
     public static void main(String[] args) {
@@ -11,12 +14,25 @@ public class RecipeParser {
             // Отправьте HTTP-запрос на URL рецепта
             Document doc = Jsoup.connect("https://www.povarenok.ru/recipes/show/47352/").get();
 
-            // Извлечение информации о рецепте
+            // название
             Element name = doc.select("h1[itemprop=name]").first();
+            //описание
             Element description = doc.select("div.article-text[itemprop=description]").first();
-          
+            //категория
+            Elements categories = doc.select("span[itemprop=recipeCategory]");
+            ArrayList<String> categoryList = new ArrayList<>();
+            for (Element category : categories) {
+                categoryList.add(category.text());
+            }
+
+
+            //вывод, удали меня
             System.out.println(name.text());
             System.out.println(description.text());
+            for (String category : categoryList) {
+                System.out.println(category);
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
