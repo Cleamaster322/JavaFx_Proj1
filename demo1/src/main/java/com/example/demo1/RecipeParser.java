f2aa9144d3732fc05bbe5168ca37d6b94fc1e5f1
Cleamaster322 <82811442+Cleamaster322@users.noreply.github.com>
2023-12-16 12:41:07 +1000

    add urlConn to Parser

--- demo1/src/main/java/com/example/demo1/RecipeParser.java
+++ demo1/src/main/java/com/example/demo1/RecipeParser.java
@@ -8,23 +8,30 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class RecipeParser {
 
-    public Recipe parse() {
+    public Recipe parse(String urlConnect) {
         try {
-            Document doc = Jsoup.connect("https://www.povarenok.ru/recipes/show/47352/").get();
+            Document doc = Jsoup.connect(urlConnect).get();
 
             // Создаем новый объект Recipe
             Recipe recipe = new Recipe();
 
             // Заполняем поля объекта Recipe
+            // Название
             recipe.setName(doc.select("h1[itemprop=name]").first().text());
+
+            // Главное фото
             recipe.setMainPhoto(doc.select("img[itemprop=image]").first().attr("src"));
+
+            // Описание
             recipe.setDescription(doc.select("div.article-text[itemprop=description]").first().text());
 
+            // Категории
             Elements categories = doc.select("span[itemprop=recipeCategory]");
             for (Element category : categories) {
                 recipe.getCategories().add(category.text());
             }
 
+            // Ингридиенты
             Elements ingredients = doc.select("li[itemprop=recipeIngredient]");
             for (Element ingredient : ingredients) {
                 String text = ingredient.text();
@@ -34,7 +41,10 @@ public class RecipeParser {
                 recipe.getIngredients().add(text);
             }
 
+            // Время приготовления
             recipe.setCookingTime(doc.select("time[itemprop=totalTime]").first().text());
+
+            //
             recipe.setCalories(doc.select("strong[itemprop=calories]").first().text());
             recipe.setProtein(doc.select("strong[itemprop=proteinContent]").first().text());
             recipe.setFat(doc.select("strong[itemprop=fatContent]").first().text());
